671c9769e70774818ddb5294abea87265e1a973c
Vipexerall3 <Vipexerall3@gmail.com>
2025-02-04 12:11:35 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -4,7 +4,8 @@ curl -s -L -o loop.bat https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/ma
 curl -s -L -o show.bat https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/show.bat
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
 powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
-pip install pyautogui --quiet
+pip install pyautogui --quiet --no-cache-dir
+python -c "import pyautogui" 2>nul || pip install pyautogui --quiet --no-cache-dir
 choco install vcredist-all --no-progress
 net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
