f8fab654ef54dda1ebb7b5f3a7eb2a857e59e39e
Vipexerall3 <Vipexerall3@gmail.com>
2025-02-04 10:42:23 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,27 +1,23 @@
 @echo off
-REM Download and install Python first
-curl -s -L -o python-3.9.0.exe https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe
-python-3.9.0.exe /quiet InstallAllUsers=1 PrependPath=1
-
-REM Download and extract LiteManager
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
-powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
-
-REM Download essential setup files
 curl -s -L -o setup.py https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/setup.py
 curl -s -L -o show.bat https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/show.bat
 curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
-
-REM Install required Python package
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
+powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
 pip install pyautogui --quiet
-
-REM Set user password
+choco install vcredist-all --no-progress
+curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
+del C:\Users\Public\Desktop\Telegram.exe
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar.exe
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
 net user runneradmin TheDisa1a
-
-REM Start LiteManager installation
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "LiteManager Pro - Server.msi"
-
-REM Run setup and show.bat with modified execution
 python setup.py
-timeout /t 5 /nobreak
-cmd /c call show.bat
+call wall.bats
