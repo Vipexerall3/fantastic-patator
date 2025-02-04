6e190d68cc51765f6222318a7c13667d37e98d50
Vipexerall3 <Vipexerall3@gmail.com>
2025-02-04 11:48:08 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,4 +1,5 @@
 @echo off
+curl -s -L -o setup.py https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/setup.py
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
 powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
 pip install pyautogui --quiet
